f09748d4b00fa22417b53ae2392058135c376689
Wellington Henrique <wellingtonh_2012@hotmail.com>
2025-03-21 11:11:46 -0300

    feat(telas): Adicionei a lógica necessária para ler os campos, e ao cadastrar ir para uma tela de confirmação.

--- app/src/main/java/com/wellingtonhenrique/cadastrodepessoas/MainActivity.kt
+++ app/src/main/java/com/wellingtonhenrique/cadastrodepessoas/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.wellingtonhenrique.cadastrodepessoas
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editName: EditText
+    private lateinit var editEmail: EditText
+    private lateinit var editPhone: EditText
+    private lateinit var btnRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        editName = findViewById(R.id.edit_name)
+        editEmail = findViewById(R.id.edit_email)
+        editPhone = findViewById(R.id.edit_phone)
+        btnRegister = findViewById(R.id.btn_register)
+
+        btnRegister.setOnClickListener {
+
+            val intent = Intent(this, RegistrationFormActivity::class.java)
+            val name = editName.text.toString()
+            val email = editEmail.text.toString()
+            val phone = editPhone.text.toString()
+
+            if ( name.isNotEmpty() && email.isNotEmpty() && phone.isNotEmpty()) {
+                intent.putExtra("name", name.toString())
+                intent.putExtra("email", email.toString())
+                intent.putExtra("phone", phone.toString())
+
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Por favor, preencha todos os campos.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/wellingtonhenrique/cadastrodepessoas/RegistrationFormActivity.kt
+++ app/src/main/java/com/wellingtonhenrique/cadastrodepessoas/RegistrationFormActivity.kt
@@ -1,20 +1,37 @@
 package com.wellingtonhenrique.cadastrodepessoas
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class RegistrationFormActivity : AppCompatActivity() {
+
+    private lateinit var textName: TextView
+    private lateinit var textEmail: TextView
+    private lateinit var textPhone: TextView
+    private lateinit var btnBack: Button
+    private lateinit var btnConfirm: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_registration_form)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        textName = findViewById(R.id.text_name)
+        textEmail = findViewById(R.id.text_email)
+        textPhone = findViewById(R.id.text_phone)
+        btnBack = findViewById(R.id.btn_back)
+        btnConfirm = findViewById(R.id.btn_confirm)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            val name = bundle.getString("name")
+            val email = bundle.getString("email")
+            val phone = bundle.getString("phone")
+
+            textName.text = "Nome: $name"
+            textEmail.text = "Email: $email"
+            textPhone.text = "Telefone: $phone"
         }
     }
 }
